ba4fccee3280478b32742ec46e3ff4bab9f5da65
Timothy McGowen <mcgowen17@yahoo.com>
2019-09-04 22:09:43 -0700

    Added the ability to get users from end points / and /:id

--- users/userRouter.js
+++ users/userRouter.js
@@ -16,12 +16,22 @@ router.get('/', (req, res) => {
         .then(users => res.status(200).json(users))
         .catch(err => {
             console.log('Error inside of get users')
-            res.status(500).json({ error: "The users info could not be retrueved." })
+            res.status(500).json({ error: "The users info could not be retrieved." })
         })
 });
 
 router.get('/:id', (req, res) => {
-
+    const { id } = req.params
+    db.getById(id)
+        .then(users => {
+            console.log(users.name)
+            if (users.name) {
+                return res.status(200).json(users)
+            }
+        })
+        .catch(err => {
+            res.status(404).json({ error: "The User with that ID doesn't exsist" })
+        })
 });
 
 router.get('/:id/posts', (req, res) => {
