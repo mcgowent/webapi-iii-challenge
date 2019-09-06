d3d9d6642a27c9465625fa72fbb2415b3d948086
Timothy McGowen <mcgowen17@yahoo.com>
2019-09-05 20:35:07 -0700

    Deploying

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // code away!
+require('dotenv').config()
+
 const express = require('express')
 const postsRouter = require('./posts/postRouter')
 const usersRouter = require('./users/userRouter')
 
+const port = process.env.PORT
 
 const server = express()
 server.use(express.json())
@@ -10,7 +13,6 @@ server.use(express.json())
 server.use('/api/posts', postsRouter)
 server.use('/api/users', usersRouter)
 
-server.listen(4000, () => {
-    console.log("Server running on port 4000")
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
-
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const db = require('./postDb')
 
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
